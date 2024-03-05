6e55ff73bbe327c384ebd2257abf4afd9af4e548
danjkim21 <dan.jkim21@gmail.com>
2024-03-05 10:36:35 -0500

    added rating stars to review list component

--- components/reviews/ReviewList.tsx
+++ components/reviews/ReviewList.tsx
@@ -94,6 +94,20 @@ const Review = ({
       )}
     >
       <div className="w-full">
+        <div className="flex items-center">
+          <svg
+            className="me-1 h-4 w-4 text-yellow-300"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 22 20"
+          >
+            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+          </svg>
+          <p className="ms-2 text-sm font-semibold text-muted-foreground">
+            <span className="">{review.rating}</span> out of 5
+          </p>
+        </div>
         <blockquote className="text-background-foreground text-left font-normal italic dark:text-white">
           <p className='before:content-["\""] after:content-["\""]'>
             {review.content}
